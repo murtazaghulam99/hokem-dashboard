7ebbb2f23ab674e35a8e751e29c46fce002d0fef
Ghulam Murtaza <murtazarb99@gmail.com>
2024-05-11 23:31:52 +0500

    first commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,7 +7,7 @@ import FinancialMetrics from "./ui/FinancialMetrics";
 const Dashboard = () => {
   return (
     <>
-      <section className="flex-grow py-4 px-2 lg:px-10 md:mr-4 md:my-5 scrollbar-hide overflow-y-auto bg-[url('./assets/background.png')] bg-no-repeat bg-cover bg-center h-screen">
+      <section className="flex-grow py-4 px-2 lg:px-10 md:mr-4 md:my-5 scrollbar-hide overflow-y-auto bg-[url('./assets/background.png')] bg-no-repeat rounded-3xl bg-cover bg-center h-screen">
         <Header />
         <Stats />
         <Analysis />
